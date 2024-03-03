9f5b4af1cb65f899d74d555d81232ac0c3aeebb1
Deng, Jianyu <kdeng@icao.int>
2024-03-03 16:19:21 -0500

    Init project folders

--- WMApp/Program.cs
+++ WMApp/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
